cf9688d446815963fbc44dbb59af95034dc6f6a7
ST10471198 <ST10471198@rcconnect.edu.za>
2026-04-05 15:23:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cybersecurity_chatbot
+{//start of namespace
+    public class Program
+    {//start of class
+        static void Main(string[] args)
+        {//start of main method
+
+
+        }//end of main method
+
+    }//end of class
+
+}//end of namespace
